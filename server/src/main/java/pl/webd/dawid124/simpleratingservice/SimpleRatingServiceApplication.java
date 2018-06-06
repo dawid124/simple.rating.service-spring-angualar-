3227f2bec77bbe16f649157bb8e40158bee6fd35
dawid124 <dawid124124@gmail.com>
2018-06-06 19:31:26 +0200

    added base spring and angular apps

---
+++ server/src/main/java/pl/webd/dawid124/simpleratingservice/SimpleRatingServiceApplication.java
@@ -0,0 +1,12 @@
+package pl.webd.dawid124.simpleratingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRatingServiceApplication.class, args);
+	}
+}
---
+++ server/src/test/java/pl/webd/dawid124/simpleratingservice/SimpleRatingServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.webd.dawid124.simpleratingservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleRatingServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
