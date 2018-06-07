33d3e0b408d6349b00b068a80e2ad97f9e4b3856
dawid124 <dawid124124@gmail.com>
2018-06-07 17:39:10 +0200

    Front - convert project to scss

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to simple-rating-service-front!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to front!');
   }));
 });
